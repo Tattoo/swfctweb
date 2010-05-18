0bd14c13c8bd092de011281fe269f62d80eaaaf0
Tatu P Kairi <tkairi@wel-69.cs.helsinki.fi>
2010-05-18 14:27:06 +0300

    added project validation, tweaked style

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,8 @@ class Project
   field :name
   field :starts_at, :type => Date
   field :ends_at, :type => Date
+
+  validates :name, :presence => true
+  validates :starts_at, :presence => true
+  validates :ends_at, :presence => true 
 end
