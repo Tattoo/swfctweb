f7cc81bfa5b35c076e3d1a7c63a560dd830b6a97
TKa <tatu.kairi@gmail.com>
2010-05-24 18:11:33 +0300

    contact form works now

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -19,7 +19,21 @@ class StaticsController < ApplicationController
   end
 
   def post_contact
-    raise params.inspect
+
+    email = params[:email]
+    message = params[:message]
+    begin
+      f = File.open("/home/tattoo/messages.txt", "a")
+      f.puts(email)
+      f.puts(Time.now)
+      f.puts("\n")
+      f.puts(message)
+      f.puts("\n\n----\n")
+      f.close
+      flash[:notice] = "Thank you!"
+    rescue
+      flash[:error] = "Something went wrong. Please try again later."
+    end
     redirect_to contact_path
   end
 
