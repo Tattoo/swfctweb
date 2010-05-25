79661f351cbd05b8069d0b30aa710b607704ebae
Tatu P Kairi <tkairi@wel-69.cs.helsinki.fi>
2010-05-25 15:11:52 +0300

    slogan generator for header, fixes issue #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+
+  before_filter :generate_slogan
+
+  def generate_slogan
+    @slogan_name = "slogans/slogan" + (1 + rand(10)).to_s + ".png"
+  end
 end
