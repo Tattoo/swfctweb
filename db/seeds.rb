8df12d13b7266152adaa769f6b01eecd21504020
Henri T J Karhatsu <karhatsu@wow-10.cs.helsinki.fi>
2010-06-02 14:13:03 +0300

    two texts to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# text fragments
+keys_and_values = [
+  ["education.text", "*Education* strives to integrate its operations in the teaching of several universities offering a wide-range of real-life data and material for university teachers' use. The global development space makes the learning experience for students something unreachable in typical university settings.\n\n*In addition* to this, the real-life development environment and requirements give the students an opportunity for significant learning gains as opposed to traditional software development projects."],
+  ["research.text", "*Software Factory's* research trains effectively PhD students, performs basic and applied research in its operating context and performs tests for evaluating different research methods. Software factory provides a context for PhD students as well as Master's students to pursue their thinking further and challenge the common wisdom.\n\n*Software Factory* is designed for allowing a multitude of different ways to collect data unseen before. Cross-disciplinary research is part of the field of software engineering. Therefore the factory has an open call for research proposals to investigate team dynamics, programming psychology and beyond to fully make use of the opportunity at hand. Software Factory's research is built upon highest ethical rules and each research endeavor shall follow the Factory's research protocol as well as the outlined research design. "],
+  ]
+
+keys_and_values.each do |pair|
+  TextFragment.create(:key => pair[0], :value => pair[1]) unless TextFragment.find(:first, :conditions => {:key => pair[0]})
+end
