c7a1089d9aaa6984e5ffa124edea4734d7d0964a
Henri T J Karhatsu <karhatsu@wow-10.cs.helsinki.fi>
2010-06-03 11:47:25 +0300

    last tf removed from seeds file (not used)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,5 @@
 # text fragments
 keys_and_values = [
-  ["research.text", "*Software Factory's* research trains effectively PhD students, performs basic and applied research in its operating context and performs tests for evaluating different research methods. Software factory provides a context for PhD students as well as Master's students to pursue their thinking further and challenge the common wisdom.\n\n*Software Factory* is designed for allowing a multitude of different ways to collect data unseen before. Cross-disciplinary research is part of the field of software engineering. Therefore the factory has an open call for research proposals to investigate team dynamics, programming psychology and beyond to fully make use of the opportunity at hand. Software Factory's research is built upon highest ethical rules and each research endeavor shall follow the Factory's research protocol as well as the outlined research design. "],
   ]
 
 keys_and_values.each do |pair|
