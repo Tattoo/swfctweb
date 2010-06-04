424af6da0b7ab2b188f56156d6bcae706462f860
Henri T J Karhatsu <karhatsu@wow-10.cs.helsinki.fi>
2010-06-04 11:12:41 +0300

    date validation and active methods to Project

--- app/models/project.rb
+++ app/models/project.rb
@@ -9,5 +9,22 @@ class Project
   validates :key, :presence => true
   validates :name, :presence => true
   validates :starts_at, :presence => true
-  validates :ends_at, :presence => true 
+  validates :ends_at, :presence => true
+  validate :dates_must_be_in_order
+
+  def active?
+    starts_at <= Date.today and ends_at >= Date.today
+  end
+
+  def self.active_project
+    # currently assumed that only one active project at a time (will change at least in Global SF)
+    Project.find(:first, :conditions => {:starts_at.lte => Time.now, :ends_at.gte => Time.now})
+  end
+
+private
+  def dates_must_be_in_order
+    if starts_at and ends_at
+      errors.add(:ends_at, "End date must be after start date") if ends_at <= starts_at
+    end
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -36,6 +36,80 @@ describe Project do
       @project.ends_at = ""
       @project.should_not be_valid
     end
+
+    it "should require that dates are in proper order" do
+      @project.starts_at = "2010-04-02"
+      @project.ends_at = "2010-04-01"
+      @project.should_not be_valid
+    end
+
+    it "should prevent one day project" do
+      @project.starts_at = "2010-04-02"
+      @project.ends_at = "2010-04-02"
+      @project.should_not be_valid
+    end
+  end
+
+  describe "active" do
+    it "is active when start date is today" do
+      @project.starts_at = Date.today
+      @project.ends_at = Date.today + 1
+      @project.should be_active
+    end
+
+    it "is active when end date is today" do
+      @project.starts_at = Date.today - 1
+      @project.ends_at = Date.today
+      @project.should be_active
+    end
+
+    it "is not active when start date is in future" do
+      @project.starts_at = Date.today + 1
+      @project.ends_at = Date.today + 2
+      @project.should_not be_active
+    end
+
+    it "is not active when end date is in past" do
+      @project.starts_at = Date.today - 2
+      @project.ends_at = Date.today - 1
+      @project.should_not be_active
+    end
+  end
+
+  describe "active_project" do
+    describe "no active projects" do
+      before do
+        Project.destroy_all
+      end
+
+      it "should return nil" do
+        Project.active_project.should == nil
+      end
+    end
+
+    # currently assumed that only one active project at a time (will change at least in Global SF)
+    describe "active project" do
+      before do
+        @valid_attrs[:name] = "Old project"
+        @valid_attrs[:starts_at] = Date.today - 20
+        @valid_attrs[:ends_at] = Date.today - 10
+        @old = Project.create(@valid_attrs)
+
+        @valid_attrs[:name] = "Active project"
+        @valid_attrs[:starts_at] = Date.today - 5
+        @valid_attrs[:ends_at] = Date.today + 10
+        @active = Project.create(@valid_attrs)
+
+        @valid_attrs[:name] = "Future project"
+        @valid_attrs[:starts_at] = Date.today + 20
+        @valid_attrs[:ends_at] = Date.today + 30
+        @future = Project.create(@valid_attrs)
+      end
+
+      it "should return the active project" do
+        Project.active_project.name.should == "Active project"
+      end
+    end
   end
 end
 
