0a1950e3296a58948ce4b13c18dcc639c8c1b1af
Henri T J Karhatsu <karhatsu@wow-10.cs.helsinki.fi>
2010-06-04 16:03:46 +0300

    no (ongoing) projects case, next project to main page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,7 @@ class IndexController < ApplicationController
 
   def show
     @active_projects = Project.active_projects
+    @future_projects = Project.future_projects
   end
 
 end
--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe IndexController do
   describe "#show" do
     before do
-      @projects = mock(Array)
+      @active_projects = mock(Array)
+      @future_projects = mock(Array)
     end
 
     it "should render show" do
@@ -11,10 +12,16 @@ describe IndexController do
       response.should render_template(:show)
     end
 
-    it "should assign project" do
-      Project.should_receive(:active_projects).and_return(@projects)
+    it "should assign active projects" do
+      Project.should_receive(:active_projects).and_return(@active_projects)
       get :show
-      assigns(:active_projects).should == @projects
+      assigns(:active_projects).should == @active_projects
+    end
+
+    it "should assign future projects" do
+      Project.should_receive(:future_projects).and_return(@future_projects)
+      get :show
+      assigns(:future_projects).should == @future_projects
     end
   end
 end
