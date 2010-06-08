3333a23d96f46cb86c7ef78610fa55ef8388afc2
Henri T J Karhatsu <karhatsu@wow-10.cs.helsinki.fi>
2010-06-08 10:37:30 +0300

    Marko's study to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,11 @@ projects = [
   {:key => "gnobles", :name => "Gnobles", :starts_at => "2010-01-18", :ends_at => "2010-03-05",
     :description => "Web platform for small companies gathering funding and for people investing in small companies.",
     :technologies => "Ruby on Rails, RSpec, Cucumber, JQuery, Postgres, TDD, BDD, Kanban",
-    :study_keys => ["gradu_henri", "gradu_elena", "psycho"]},
+    :study_keys => ["psycho", "waste_marko", "gradu_henri", "gradu_elena"]},
   {:key => "yfactor", :name => "Y-Factor", :starts_at => "2010-03-15", :ends_at => "2010-05-07",
     :description => "Generic recommendation plug-in for online stores. \"People who bought this bought also that.\"",
     :technologies => "Ruby on Rails, Redis, Cucumber, JQuery, MySQL, Kanban",
-    :study_keys => ["gradu_henri", "psycho"]},
+    :study_keys => ["psycho", "waste_marko", "gradu_henri"]},
   {:key => "planesweep", :name => "Planesweep", :starts_at => "2010-05-17", :ends_at => "2010-07-02",
     :description => "Airplane cleaning management system.",
     :technologies => "Scala, Lift, CouchDB, Javascript, TDD, Kanban",
@@ -33,6 +33,9 @@ studies = [
     :researchers => "Markku Verkasalo, Sointu Leikas, Jan-Erik Lonnqvist, Ville Ilmarinen (Institute of Behavioural Sciences, Department of Psychology, University of Helsinki)",
     :research_methods => "Self-reports of personality, work stress, mood, and work satisfaction via questionnaires. Experience sampling of self-report ratings of social interactions via palmtop computers. Video observation.",
     :project_keys => ["gnobles", "yfactor", "planesweep"]},
+  {:key => "waste_marko", :title => "Finding waste", :researchers => "Marko Ikonen",
+    :research_methods => "Direct observation, thematic interviews",
+    :project_keys => ["gnobles", "yfactor"]},
 ]
 
 Study.destroy_all # remove this if an admin page for studies is created
